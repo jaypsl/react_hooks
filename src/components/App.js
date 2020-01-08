e154598f087131a336082599aca80c89a4547527
jaypal solanki <solankijp94@gmail.com>
2020-01-08 10:00:54 -0800

    pr1

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,20 @@
-import React,{useState} from 'react';
-import List from './List';
-import UserList from './UserList';
+import React, { useState } from "react";
+import List from "./List";
+import UserList from "./UserList";
 
 const App = () => {
-    // this.state = {resource : 'posts'};
-    const [resource, setResource] = useState('posts');
+  const [resource, setResource] = useState("posts");
 
-    return(
-       
-        <div>  
-            <UserList/>
-            <div>
-            <button onClick={()=>setResource('posts')}>Posts</button>
-            <button onClick={()=>setResource('todos')}>Todos</button>
-            </div>
-            <List resource={resource}/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <UserList />
+      <div>
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
+      </div>
+      <List resource={resource} />
+    </div>
+  );
+};
 
 export default App;
